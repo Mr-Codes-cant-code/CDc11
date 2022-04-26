a8e37a862796703411558a3858bccda248cc5d0a
Khalid Azmatullah <khalidazy.2008@gmail.com>
2022-04-26 15:47:47 +0530

    Placement changes

---
+++ CDc10/lib/lang/load-lang.rb
@@ -0,0 +1,31 @@
+## UID Explanation
+=begin
+ #XXXXXXXXX
+ ^---___===
+ ^ = "Caller"
+ - = "Type of lang"
+ _ = "Unique alphabet Identification Code"
+ = = "Unique numerical Identification Code"
+ ex = #632xuc972
+=end
+
+print("
+  Loading languages")
+  trash_count = 10000000
+counter = 0
+while counter != trash_count
+  counter+=1
+end
+print(".")
+trash_count = 10000000
+counter = 0
+while counter != trash_count
+  counter+=1
+end
+print(".")
+trash_count = 10000000
+counter = 0
+while counter != trash_count
+  counter+=1
+end
+puts(".")
--- CDc10/loader.rb
+++ CDc10/loader.rb
@@ -1,6 +1,5 @@
 
-require_relative ("lang/load-lang.rb")
-require_relative ("lib/uid-direct/load-uid.rb")
+require_relative ("lib/lang/load-lang.rb")
 require_relative ("runner.rb")
 
 ## end
